4b24947f550b4ba0ad1d56df4896b7e66c7d37aa
blokhinamaria <blokhinamariyayu@gmail.com>
2026-01-22 15:18:15 -0500

    updated tsconfig files to config files for vercel serveless func

--- api/events/index.ts
+++ api/events/index.ts
@@ -1,5 +1,5 @@
 import type {VercelRequest, VercelResponse} from '@vercel/node'
-import { getMonthEvents } from '../../server/controllers/eventsController'
+import { getMonthEvents } from '../../server/controllers/eventsController.js'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
